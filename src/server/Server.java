1f34ea33696561dbf32a6d68995ab5c3027a3864
SOuellette22 <ouellettese@merrimack.edu>
2025-12-08 13:48:01 -0500

    Feat: added the ant build file and the basic usage of the Server file

---
+++ src/server/Server.java
@@ -0,0 +1,138 @@
+package server;
+
+import merrimackutil.cli.LongOption;
+import merrimackutil.cli.OptionParser;
+import merrimackutil.json.InvalidJSONException;
+import merrimackutil.json.JsonIO;
+import merrimackutil.json.types.JSONObject;
+import merrimackutil.json.types.JSONType;
+import merrimackutil.util.Tuple;
+import merrimackutil.net.Log;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InvalidObjectException;
+
+class Server {
+
+    public static String configFile = "config.json";
+
+    public static int port;
+    public static String databaseDir;
+    public static int maxConnections;
+    public static String log;
+
+    /**
+     * Prints the usage message for the server application
+     */
+    public static void usage() {
+        System.out.println("Usage: ");
+        System.out.println("  server");
+        System.out.println("  server --config <file>");
+        System.out.println("  server --help");
+        System.out.println("Options: ");
+        System.out.printf("  %-15s %-20s\n", "-c, --config", "Specify configuration file (default: config.json)");
+        System.out.printf("  %-15s %-20s\n", "-h, --help", "Display this help message");
+    }
+
+    /**
+     * Processes command-line arguments
+     *
+     * @param args the command-line arguments
+     */
+    public static void processArgs(String[] args) {
+        OptionParser parser;
+
+        LongOption[] opts = new LongOption[2];
+        opts[0] = new LongOption("config", true, 'c');
+        opts[1] = new LongOption("help", false, 'h');
+
+        parser = new OptionParser(args);
+        parser.setLongOpts(opts);
+        parser.setOptString("c:h");
+
+        Tuple<Character, String> currOpt;
+        boolean doHelp = false;
+
+        while (parser.getOptIdx() != args.length) {
+            currOpt = parser.getLongOpt(false);
+            switch (currOpt.getFirst()) {
+                case 'c':
+                    configFile = currOpt.getSecond();
+                    break;
+                case 'h':
+                    doHelp = true;
+                    break;
+                default:
+                    System.out.println("Invalid option " + currOpt.getFirst());
+                    usage();
+                    System.exit(1);
+            }
+        }
+
+        if (doHelp) {
+            usage();
+            System.exit(0);
+        }
+
+        File file = new File(configFile);
+        if (!file.exists() || file.length() == 0) {
+            System.out.println("No valid config file provided!!!");
+            System.exit(1);
+        }
+
+        try {
+            deserialize(JsonIO.readObject(file));
+        } catch (InvalidObjectException | FileNotFoundException | InvalidJSONException e) {
+            System.err.println("Error reading config file!!!");
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args) {
+        if (args.length > 1)  {
+            usage();
+            System.exit(0);
+        }
+
+        processArgs(args);
+
+        Log serverLog;
+
+        try {
+            serverLog = new Log(log, "ServerLog");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        serverLog.log("Starting server on port " + port);
+        serverLog.log("Using database directory: " + databaseDir);
+        serverLog.log("Max connections set to: " + maxConnections);
+
+    }
+
+    /**
+     * Helper method for deserializing the JSON config file
+     *
+     * @param jsonType the JSONType to deserialize
+     * @throws InvalidObjectException if the JSONType is not a valid JSONObject
+     */
+    private static void deserialize(JSONType jsonType) throws InvalidObjectException {
+        if (!(jsonType instanceof JSONObject)) {
+            throw new InvalidObjectException("Config file is not a valid JSON object");
+        }
+
+        JSONObject obj = (JSONObject) jsonType;
+
+        obj.checkValidity(new String[]{"port", "db_dir", "max_connections"});
+
+        if (obj.containsKey("log")) {
+            log = obj.getString("log");
+        }
+
+        port = obj.getInt("port");
+        databaseDir = obj.getString("db_dir");
+        maxConnections = obj.getInt("max_connections");
+
+    }
+}
