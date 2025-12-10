f5d2e83d786a6f221ceecd06e534d67953e02176
SOuellette22 <ouellettese@merrimack.edu>
2025-12-10 17:17:00 -0500

    Feat: added the basic message and the message socket and made sure they were implemented

--- src/common/MessageSocket.java
+++ src/common/MessageSocket.java
@@ -1,4 +1,92 @@
 package common;
 
-public class MessageSocket {
+import common.messages.Message;
+import merrimackutil.json.InvalidJSONException;
+import merrimackutil.json.JsonIO;
+import merrimackutil.json.types.JSONObject;
+
+import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * Wraps socket class for use to send and receive DHT Messages
+ */
+public class MessageSocket extends Socket {
+
+    // ----- PRIVATE FIELDS ----- //
+    private Scanner recv;
+    private PrintWriter send;
+
+    /**
+     * Creates Message Socket from a socket
+     *
+     * @note Useful for ServerSocket.accept();
+     *
+     * @throws IOException Throws if IOStreams cannot be established
+     */
+    public MessageSocket(Socket socket) throws IOException {
+        super(); // Call parent class (Socket)
+
+        try {
+            this.recv = new Scanner(socket.getInputStream());
+            this.send = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            System.err.println("Message socket could not get IO streams setup");
+            throw e; // Rethrow for consumer to handle
+        }
+    }
+
+    /**
+     * Creates Message Socket from address and port
+     *
+     * @throws IOException Throws if connection cannot be established
+     */
+    public MessageSocket(String addr, int port) throws IOException {
+        super(addr, port); // Call parent class (Socket)
+
+        try {
+            this.recv = new Scanner(this.getInputStream());
+            this.send = new PrintWriter(this.getOutputStream(), true);
+        } catch (IOException e) {
+            System.err.println("Message socket could not get IO streams setup");
+            throw e; // Rethrow for consumer to handle
+        }
+    }
+
+    /**
+     * This sends a message to the connected socket
+     */
+    public void sendMessage(Message msg) {
+        JsonIO.writeSerializedObject(msg, send);
+    }
+
+    /**
+     * Receives a message from the connected socket.
+     *
+     * @return The received {@link Message} object.
+     * @throws InvalidJSONException If the received data cannot be parsed as a valid JSON object.
+     * @throws InvalidObjectException
+     */
+    public Message getMessage() throws InvalidJSONException, InvalidObjectException {
+
+        String serializedMsg = recv.nextLine();
+
+        JSONObject obj = JsonIO.readObject(serializedMsg);
+
+        Message msg = new Message(obj);
+
+        switch (msg.getType()) {
+            // TODO: add other message types here as they are implemented
+            default -> {
+                throw new IllegalArgumentException("Unknown message type: " + msg.getType());
+            }
+        }
+
+    }
+
+
 }
+
--- src/common/messages/Message.java
+++ src/common/messages/Message.java
@@ -1,4 +1,99 @@
 package common.messages;
 
-public class Message {
+import merrimackutil.json.JSONSerializable;
+import merrimackutil.json.types.JSONObject;
+import merrimackutil.json.types.JSONType;
+
+import java.io.InvalidObjectException;
+
+public class Message implements JSONSerializable {
+
+    private String type;
+    private String header;
+    private int cseq;
+
+    /**
+     * Constructor creates a new message from parameters
+     */
+    public Message (String type, String header, int cseq) {
+        this.type = type;
+        this.header = header;
+        this.cseq = cseq;
+    }
+
+    /**
+     * Constructor creates a new message object from JSONobject by deserializing it
+     *
+     * @param JSONMessage JSONObject representing the message
+     */
+    public Message(JSONObject JSONMessage) {
+        try {
+            deserialize(JSONMessage);
+        } catch (InvalidObjectException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    /**
+     * Get message type
+     *
+     * @return String of message type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Get message header
+     *
+     * @return String of message header
+     */
+    public String getHeader() {
+        return header;
+    }
+
+    /**
+     * Get message cseq
+     *
+     * @return int of message cseq
+     */
+    public int getCseq() {
+        return cseq;
+    }
+
+    /**
+     * Deserialize JSONType into Message object
+     *
+     * @param jsonType JSONType to deserialize
+     * @throws InvalidObjectException if jsonType is not a valid Message representation
+     */
+    @Override
+    public void deserialize(JSONType jsonType) throws InvalidObjectException {
+
+        if (!(jsonType instanceof JSONObject)) {
+            throw new InvalidObjectException("Expected JSONObject for Message deserialization");
+        }
+
+        JSONObject obj = (JSONObject) jsonType;
+
+        obj.checkValidity(new String[]{"type", "header", "cseq"});
+
+        this.type = obj.getString("type");
+        this.header = obj.getString("header");
+        this.cseq = obj.getInt("cseq");
+    }
+
+    /**
+     * Serialize Message object into JSONType
+     *
+     * @return JSONType representation of Message
+     */
+    @Override
+    public JSONType toJSONType() {
+        JSONObject obj = new JSONObject();
+        obj.put("type", type);
+        obj.put("header", header);
+        obj.put("cseq", cseq);
+        return obj;
+    }
 }
