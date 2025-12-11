a11f3e5c1e4927131d53f7525cadda801058626f
SOuellette22 <ouellettese@merrimack.edu>
2025-12-11 17:43:54 -0500

    feat: added client test code

--- src/client/Client.java
+++ src/client/Client.java
@@ -5,4 +5,18 @@ import common.messages.OptionMessage;
 
 class Client {
 
+    public static void main(String[] args) {
+        System.out.println("Client started.");
+
+        try {
+            MessageSocket messageSocket = new MessageSocket("127.0.0.1", 5000);
+
+            OptionMessage msg = new OptionMessage("client1", 1, "DESCRIBE SETUP PLAY PAUSE TEARDOWN");
+            System.out.println(msg);
+            messageSocket.sendMessage(msg);
+
+        } catch (Exception e) {
+            System.err.println("Error establishing message socket: " + e.getMessage());
+        }
+    }
 }
