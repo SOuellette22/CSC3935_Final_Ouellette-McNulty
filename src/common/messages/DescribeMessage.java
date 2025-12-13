8d203f6cd48144488473c0528300e158616edc6e
mcnultye <157040192+mcnultye@users.noreply.github.com>
2025-12-13 15:45:43 -0500

    Creating describe message

--- src/common/messages/DescribeMessage.java
+++ src/common/messages/DescribeMessage.java
@@ -1,5 +1,66 @@
 package common.messages;
 
-public class DescribeMessage {
+public class DescribeMessage extends Message{
     
+    private String accept;
+
+    /**
+     * Constructor creates a new DESCRIBE message from parameters
+     *
+     * @param header This is the header of the message
+     * @param cseq This is the cseq of the message
+     * @param accept This is the accepted content
+     */
+    public DescribeMessage(String header, int cseq, String accept) {
+        super("DESCRIBE", header, cseq);
+        this.accept = accept;
+    }
+
+    /**
+     * Constructor creates a new DESCRIBE message from a message string
+     * 
+     * @param messageString This is the messageString
+     */
+    public DescribeMessage(String messageString) {
+        super(messageString);
+
+        if (!(this.getType().equals("DESCRIBE"))) {
+            throw new IllegalArgumentException("Invalid message type for DescribeMessage: " + this.getType());
+        }
+
+        String[] lines = messageString.split("\r\n");
+
+        for (String line : lines) {
+            if (line.startsWith("Accept: ")) {
+                this.accept = line.substring(8).trim();
+            }
+        }
+
+        //Validate required fields
+        if (this.accept == null || this.accept.isEmpty()) {
+            throw new IllegalArgumentException("Accept field is required in DESCRIBE message");
+        }
+    }
+
+    /**
+     * Get accept header
+     * 
+     * @return String of accept header
+     */
+    public String getAccept() {
+        return accept;
+    }
+
+    /**
+     * Override toString to include accept header
+     */
+    @Override
+    public String toString() {
+        if (messageString != null) {
+            return messageString;
+        } else {
+            String baseString = super.toString() + "Accept: " + accept + "\r\n";
+            return baseString + "\r";
+        }
+    }
 }
